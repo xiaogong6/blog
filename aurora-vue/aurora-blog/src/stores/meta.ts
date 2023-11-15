f42eb60010fee54ef28b4f2142742ed63da994cc
xiongke <917634854@qq.com>
2023-11-15 23:19:42 +0800

    fix:前端

--- aurora-vue/aurora-blog/src/stores/meta.ts
+++ aurora-vue/aurora-blog/src/stores/meta.ts
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 export const useMetaStore = defineStore('metaStore', {
   state: () => {
     return {
-      title: '花未眠的个人博客'
+      title: '城北花开的奇思妙想'
     }
   }
 })
